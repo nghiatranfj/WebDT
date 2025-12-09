7625e12bbb718dcc09c958ac6619ba12af53cf52
LvNhatQuan <nhatquanb@gmail.com>
2025-12-09 20:57:05 +0700

    ViewComponents

---
+++ WebD&T/ViewComponents/MenuCategoryViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebDT.DAL;
+using WebDT.Models;
+
+namespace WebD_T.ViewComponents
+{
+    public class MenuCategoryViewComponent : ViewComponent
+    {
+        // query SQL 
+        CategoryDAL categoryDAL = new CategoryDAL();
+        public IViewComponentResult Invoke()
+        { 
+            List<CategoryMenu> categoryMenus = new List<CategoryMenu>();
+
+            categoryMenus = categoryDAL.getAllWithCount(); 
+            return View("Default", categoryMenus);
+        }
+    }
+}
